3a2d98d57152ac1407081d934243ae7702f46dd5
palpatinezw <emperorpalpatinezw@gmail.com>
2026-03-15 18:35:21 +0100

    Trolling

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { getDocs } from './auxiliaries/extract.js'
 import cors from 'cors'
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(cors())
 
